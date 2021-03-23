a4b267ffa8a587e377c5feaedc8c542260b6830a
bhuone <11137745+bhuone-garbu@users.noreply.github.com>
2021-03-23 10:50:25 +0000

    adding a upload demo

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,8 @@ if (
 
 // or just use cors 😅
 app.use((_, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
